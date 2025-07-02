b93ba492cdf8f991fa8628710f8d14548a21c32d
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-07-02 18:28:13 +0530

    implement socket.io in server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import http from 'http';
 import app from './app.js';
+import { initializeSocket } from './config/socket.js'
 
 const server = http.createServer(app)
 const PORT = process.env.PORT || 3000;
+
+initializeSocket(server)
 
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
